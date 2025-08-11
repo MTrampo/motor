43c847ea0b3da0eff836e427f0ab26e5bcb18ceb
vinicius <vinnicius4@hotmail.com>
2025-08-11 11:22:46 -0300

    prompt para instalação

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,24 @@ import expenses from "@/commons/data/expenses.json"
 import auctioned from "@/commons/data/auctioned.json"
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import InstallPrompt from "@/pwa/install-prompt";
 
 export default function Home() {
   const total = expenses.reduce((a, c) => a + c.valor, 0)
   return (
     <main className="mx-auto px-6 py-12 sm:py-24 max-w-5xl md:max-w-7xl">
 
-      <Button variant="ghost" asChild className="mb-10">
+      <div className="flex items-center justify-between mb-10">
+        <Button variant="ghost" asChild className="mb-10">
         <Link prefetch href='/dashboard' className="bg-blue-500 p-2 text-primary-foreground shadow-xs hover:bg-blue-500/90 ">
           NOVA VERSÃO
         </Link>
       </Button>
+
+      <div className="block">
+        <InstallPrompt/>
+      </div>
+      </div>
       
 
       <h2>Arrematado</h2>
