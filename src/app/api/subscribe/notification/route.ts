4f01dd33c8a0e014c093c5c5143147d5f73bc428
vinicius <vinnicius4@hotmail.com>
2025-09-21 11:11:58 -0300

    ajuste no route

--- src/app/api/subscribe/notification/route.ts
+++ src/app/api/subscribe/notification/route.ts
@@ -1,18 +1,18 @@
-// export async function POST(req: Request) {
-//   try {
-//     const subscription = await req.json();
+export async function POST(req: Request) {
+  try {
+    const subscription = await req.json();
 
-//     if (!subscription || !subscription.endpoint) {
-//       return Response.json({ message: 'Dados de inscrição inválidos.' }, { status: 400 });
-//     }
+    if (!subscription || !subscription.endpoint) {
+      return Response.json({ message: 'Dados de inscrição inválidos.' }, { status: 400 });
+    }
 
-//     // Adiciona a inscrição como um novo documento na coleção 'subscriptions'
-//     const docRef = await db.collection('subscriptions').add(subscription);
-//     console.log("Inscrição salva com sucesso, ID:", docRef.id);
+    // // Adiciona a inscrição como um novo documento na coleção 'subscriptions'
+    // const docRef = await db.collection('subscriptions').add(subscription);
+    // console.log("Inscrição salva com sucesso, ID:", docRef.id);
     
-//     return Response.json({ success: true, message: 'Inscrição salva com sucesso.' }, { status: 200 });
-//   } catch (error) {
-//     console.error("Erro ao salvar a inscrição:", error);
-//     return Response.json({ message: 'Erro interno do servidor.' }, { status: 500 });
-//   }
-// }
+    return Response.json({ success: true, message: 'Inscrição salva com sucesso.' }, { status: 200 });
+  } catch (error) {
+    console.error("Erro ao salvar a inscrição:", error);
+    return Response.json({ message: 'Erro interno do servidor.' }, { status: 500 });
+  }
+}
